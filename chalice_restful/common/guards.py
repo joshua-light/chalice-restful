2967bf67765758d8422073f54fad223c5a815027
JoshuaLight <ashkariel.eter@gmail.com>
2020-01-22 17:36:59 +0200

    Cleaned up comments in `Ensure` class.

--- chalice_restful/common/guards.py
+++ chalice_restful/common/guards.py
@@ -11,28 +11,28 @@ class Ensure:
         self.target = target
 
     def is_class(self):
-        """ Checks whether the target is a class. """
+        """ Ensures that the target is a class. """
 
         assert isclass(self.target), \
             f'Expected {str(self.target)} ' + \
             f'to be a class'
 
     def is_not(self, x: type):
-        """ Checks whether the target is not equal to the specified value. """
+        """ Ensures that the target is not equal to the specified value. """
 
         assert self.target != x, \
             f'Expected {str(self.target)} ' + \
             f'to not be {str(x)}'
 
     def is_subclass_of(self, x: type):
-        """ Checks whether the target type is subclass of the specified. """
+        """ Ensures that the target type is subclass of the specified. """
 
         assert issubclass(self.target, x), \
             f'Expected {str(self.target)} ' + \
             f'to be a subclass of {str(x)}'
 
     def has_attribute(self, attribute: str):
-        """ Checks whether the target type has attribute
+        """ Ensures that the target type has attribute
             of the specified name.
         """
 
@@ -41,7 +41,7 @@ class Ensure:
             f'to have a {attribute} attribute'
 
     def has_any_attribute(self, of: Iterable[str]):
-        """ Checks whether the target type has any
+        """ Ensures that the target type has any
             of the specified attributes.
         """
 
@@ -52,7 +52,7 @@ class Ensure:
             f'of the {of} attributes'
 
     def starts_with(self, prefix: str):
-        """ Checks whether the target string starts with
+        """ Ensures that the target string starts with
             the specified prefix.
         """
 
