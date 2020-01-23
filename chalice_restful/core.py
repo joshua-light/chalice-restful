5d79f537c2ecf38d3dbde2af22421ab360c62799
JoshuaLight <ashkariel.eter@gmail.com>
2020-01-23 18:40:37 +0200

    Enhanced docs for `Resource` class.

--- chalice_restful/core.py
+++ chalice_restful/core.py
@@ -37,22 +37,50 @@ def api_key_required():
     a `api_key_required=True` attribute to a decorated function or class.
 
     When it's used, all incoming requests to an endpoint or resource
-    should contain `x-api-key` header with valid value from AWS API Gateway.
+    should contain a `x-api-key` header with valid key from API Gateway.
     """
 
 
 class Resource:
-    """ Rerpresents a resource or a collection of resources
-        which define handlers for `GET`, `POST`, etc. HTTP-calls.
+    """A resource or a collection of resources.
 
-        :examples:
-            @route('v1/items')
-            class Items:
-                def get(): ...
-                def put(): ...
-                def post(): ...
-                def patch(): ...
-                def delete(): ...
+    Represents a bunch of functions for handling different HTTP-requests.
+
+    Subclasses of this class should define a `route` class attribute
+    (either directory or using `route` decorator):
+        a) directly:
+        class Items:
+            route = '/v1/items'
+            ...
+
+        b) with decorator:
+        @route('/v1/items')
+        class Items:
+            ...
+
+    Handlers in the subclasses of the `Resources` are defined as a functions
+    with lowercased HTTP-methods names:
+        @route('/v1/items')
+        class Items:
+            def get(): ...  # Will handle GET request.
+            def put(): ...  # Will handle PUT request.
+
+    Various configs can be used to enhance the behaviour of the endpoints.
+    For example, one could add the `cors` decorator to the `Items` class:
+        @route('/v1/items')
+        @cors
+        class Items:
+            def get(): ...
+            def put(): ...
+
+    This will make the `get` and `put` endpoints to allow cross domain access.
+
+    On the other hand, it's possible to enable CORS only for specific endpoint:
+        @route('/v1/items')
+        class Items:
+            @cors
+            def get(): ...
+            def put(): ...
     """
     ...
 
