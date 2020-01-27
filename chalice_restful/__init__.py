9f1a3323df1bae2d5dbf849b8147be7a96a97c32
JoshuaLight <ashkariel.eter@gmail.com>
2020-01-27 14:08:02 +0200

    Fixed `common` module.

---
+++ chalice_restful/__init__.py
@@ -0,0 +1,4 @@
+from .core import Api, Resource, route, cors, api_key_required
+from .common import guards
+from .configs import config, flag, only_classes, only_functions
+from .authorization import authorizer
